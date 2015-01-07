edae82fc8c2a82eaa2754f10fef5a715a9cd3d7b
jiwon <jiwon@jiwon-pc>
2015-01-07 17:54:31 +0900

    ServerManager 구현중...

--- GCAsset/Assets/ServerManager.cs
+++ GCAsset/Assets/ServerManager.cs
@@ -13,26 +13,51 @@ public class ServerManager : MonoBehaviour {
 	IPAddress mIPAddress;
 	int mPort;
 	Socket mServerSocket;
+	Socket[] mClientSockets;
 	Thread mServerThread;
 	IPEndPoint mServerEndPoint;
 
 	/**
-	 * 루프백 아이피오 할당될 포트의 최소갑과 최대값
+	 * 할당될 포트의 최소갑과 최대값 및 여러 서버에 관련된 설정 변수들
 	 */
-	string loopback = "127.0.0.1";
 	int minPort = 6000, maxPort = 7000;
-
+	int maxPlayer = 5;
+	/**
+	 * 서버 초기화 작업을 수행한다.
+	 * 랜덤 포트 생성, 소켓 생성, 
+	 */
 	void initSever(){
-		mIPAddress = IPAddress.Parse(loopback);
+		Debug.Log ("initServer");
 		mPort = Random.Range (minPort, maxPort);
-		mServerSocket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-		mServerEndPoint = new IPEndPoint (mIPAddress, mPort);
 
+	}
+
+	/**
+	 * 생성된 소켓을 실제 서버에 바인딩하고 Accept하는 스레드를 생성한다.
+	 */
+	void startServer(){
+		Debug.Log ("startServer");
+
+		if (mServerThread != null) {
+			Debug.Log("Server already started");
+			return;
+		}
+
+		mServerSocket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		mServerEndPoint = new IPEndPoint (IPAddress.Any, mPort);
+		mServerSocket.Bind (mServerEndPoint);
+		mServerSocket.Listen (maxPlayer);
+		mServerThread = new Thread (waitPlayer);
+		mServerThread.Start ();
+	}
+
+	void waitPlayer(){
+		mServerSocket.Accept ();
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("start");
 	}
 	
 	// Update is called once per frame
