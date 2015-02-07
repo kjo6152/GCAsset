05d523bde1c4aaa2f4f626232490df54ac2e4a21
jiwon <jiwon@192.168.0.8>
2015-02-08 04:14:55 +0900

    클라이언트 스크립트 추가 -> 미리 컴파일할때 코드가 있어야함

---
+++ GCAsset/Assets/GCServer/Scripts/Controller/UserScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("UserScript : Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("UserScript : Update");
+	}
+
+    public void onClick()
+    {
+        Debug.Log("UserScript : onClick");
+    }
+}
